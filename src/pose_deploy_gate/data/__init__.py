af50b325a64085bf4e3fd41f3eb627230a0d80c5
Kamil Prokop <154501726+Kamil-J-Prokop@users.noreply.github.com>
2026-06-08 15:32:25 +0200

    feat(data) updating public API and adding tests

--- src/pose_deploy_gate/data/__init__.py
+++ src/pose_deploy_gate/data/__init__.py
@@ -3,6 +3,7 @@
 from pose_deploy_gate.data.datasource import FileDataSource
 from pose_deploy_gate.data.exceptions import DataSourceError, InputDirectoryError, NoInputFilesError
 from pose_deploy_gate.data.factory import create_data_source
+from pose_deploy_gate.data.file_discovery import discover_files
 
 __all__ = [
     "DataSourceError",
@@ -10,4 +11,5 @@ __all__ = [
     "InputDirectoryError",
     "NoInputFilesError",
     "create_data_source",
+    "discover_files",
 ]
