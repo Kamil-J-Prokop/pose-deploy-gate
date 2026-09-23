c6ffb141c4c9e52c7bd608b2796747a281cdd14f
Kamil Prokop <154501726+Kamil-J-Prokop@users.noreply.github.com>
2026-09-23 13:08:59 +0200

    feat(metrics): expose metrics public API

--- src/pose_deploy_gate/metrics/__init__.py
+++ src/pose_deploy_gate/metrics/__init__.py
@@ -1,1 +1,11 @@
 """Public export of metrics module."""
+
+from .engine import MetricsEngine
+from .result import ErrorRateMetrics, LatencyMetrics, MetricsResult
+
+__all__ = [
+    "ErrorRateMetrics",
+    "LatencyMetrics",
+    "MetricsEngine",
+    "MetricsResult",
+]
